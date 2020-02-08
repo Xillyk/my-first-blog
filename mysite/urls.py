0b4bafbb49d19a1c0d0299215d24d6b0924fd2bc
Xillyk <xillyk@hotmail.com>
2020-02-08 20:21:13 +0000

    Changed the HTML for the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,9 +1,7 @@
-from django.urls import include, path
 from django.contrib import admin
-from django.views.generic import RedirectView
+from django.urls import path, include
 
 urlpatterns = [
-    path('todo/', include('todo.urls')),
     path('admin/', admin.site.urls),
-    path('',  RedirectView.as_view(url='/todo/')),
+    path('', include('blog.urls')),
 ]
